503d1c88de1675a945f5501f4334045c637c2267
Palito <axelllobet76@gmail.com>
2026-02-10 14:09:44 -0300

    V-2.1.1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,18 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { AuthService } from './service/auth.service';
 import { firstValueFrom } from 'rxjs';
-
-function initializeAuth(authService: AuthService) {
-  return () => {
-    return firstValueFrom(authService.refresh$())
-  }
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeAuth,
-      deps: [AuthService],
-      multi: true,
-    },
+    provideAppInitializer (() => {
+      const authService = inject(AuthService);
+      return firstValueFrom(authService.refresh$()); 
+    })
   ],
 }
